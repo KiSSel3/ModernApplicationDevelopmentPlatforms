5eda0750afc380d30a72225da0ae0a441e42655a
KiSSel3 <andreyk042004@gmail.com>
2023-09-25 23:44:11 +0300

    First Lab

---
+++ Web_153501_Kiselev/Web_153501_Kiselev/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_153501_Kiselev.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
